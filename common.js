30323f3064ab7d9f8db0cef82dc795a698eb06ce
JonahBolin <jonah.bolin17@gmail.com>
2025-01-03 20:33:47 +0100

    Påbörjade funktionen för Sum of all (sum)
    Tog ut header-elementen ur funktionen då de behövdes tillgängliga i det globala scopet för att hämta buttonCreate i sum/index.js. I sum/index.js påbörjade jag en funktion vars syfte är att returnera en summa av talen i allCurrentCells.

--- common.js
+++ common.js
@@ -1,6 +1,26 @@
 const body = document.querySelector("body");
 const main = document.querySelector("main");
 
+const header = document.createElement("header");
+header.classList.add("header");
+
+body.insertBefore(header, main);
+
+const paragraphInHeader = document.createElement("p");
+paragraphInHeader.textContent = "How many numbers in the grid?";
+
+const inputInHeader = document.createElement("input");
+inputInHeader.type = "text";
+inputInHeader.value = "";
+inputInHeader.classList.add("headerInput");
+
+const buttonCreate = document.createElement("button");
+buttonCreate.classList.add("buttonCreate");
+buttonCreate.textContent = "Create";
+
+header.appendChild(paragraphInHeader);
+header.appendChild(inputInHeader);
+header.appendChild(buttonCreate);
 
 // Funktion som skapar randomNumber
 function createRandomNumber() {
@@ -19,37 +39,19 @@ function createHomePageLink() {
     nav.appendChild(homePageLink);
 }
 
-createHomePageLink();
-
 //Funktion som skapar elementen i header (<p>, <input>, <button>)
-function createElementsInHeader() {
-    const header = document.createElement("header");
-    header.classList.add("header");
+/*function createElementsInHeader() {
+   
 
-    body.insertBefore(header, main);
+}*/
 
-    const paragraphInHeader = document.createElement("p");
-    paragraphInHeader.textContent = 'How many numbers in the grid?';
 
-    const inputInHeader = document.createElement("input");
-    inputInHeader.type = 'text';
-    inputInHeader.classList.add("headerInput");
-
-    const buttonCreate = document.createElement("button");
-    buttonCreate.classList.add("buttonCreate");
-    buttonCreate.textContent = 'Create';
-
-    header.appendChild(paragraphInHeader);
-    header.appendChild(inputInHeader);
-    header.appendChild(buttonCreate);
-
-    buttonCreate.addEventListener("click", function () {
-        const value = parseInt(inputInHeader.value, 10);
-        console.log(value);
-        //anrop till createGrid
-        createGrid(value);
-    })
-}
+buttonCreate.addEventListener("click", function () {
+    const value = parseInt(inputInHeader.value, 10);
+    console.log(value);
+    //anrop till createGrid
+    createGrid(value);
+})
 
 //funktion som skapar grid-nätet
 
@@ -66,6 +68,7 @@ function createGrid(amountOfCells) {
     gridContainer.innerHTML = "";
 
     for (let i = 0; i < amountOfCells; i++) {
+
         const cell = document.createElement("div");
         gridContainer.appendChild(cell);
 
@@ -74,3 +77,12 @@ function createGrid(amountOfCells) {
         cell.textContent = randomNumber;
     }
 }
+
+// anrop / event-listeners
+createHomePageLink();
+
+/*document.addEventListener("DOMContentLoaded", function () {
+    createElementsInHeader();
+})*/
+
+
--- sum/index.js
+++ sum/index.js
@@ -1,6 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
     createElementsInHeader();
     createGridShell(95);
+let ButtonCreate = document.querySelector(".buttonCreate");
+console.log(ButtonCreate);
+
+function sumAllCurrentCells() {
+    const allCurrentCells = document.querySelectorAll(".gridCells");
+    console.log(allCurrentCells);
+}
+
+
+
+
+
+
+// anrop
+ButtonCreate.addEventListener("click", function () {
+    sumAllCurrentCells();
 })
 
 document.addEventListener("DOMContentLoaded", () => {
