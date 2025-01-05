39db0d7a846c7dcac662b81f47925e73b802b6c6
JonahBolin <jonah.bolin17@gmail.com>
2025-01-05 13:37:32 +0100

    SumOfMarked + cellEventColor (sum)
    Funktionen sumMarkedDivsAndChangeTheirColor anropas i event-listener för create-knappen. I funktionen hämtas en nodelist av alla celler (divar) som skapats. For-of-loopen används för att kunna nå varje enskild cell och om ett klick på den sker så ska förändringar för just den cellen ske, dvs, få en annan färg, och sumOfMarked tilldelas ett nytt värde, som blir textinnehållet i pSumOfMarked. OBS! Någon justering fattas.

--- sum/index.js
+++ sum/index.js
@@ -25,6 +25,22 @@ function sumAllCurrentCells() {
     return sumOfAll;
 }
 
+function sumMarkedDivsAndChangeTheirColor() {
+
+    let sumOfMarked = 0;
+    let listOfNumDivs = document.querySelectorAll(".gridCells");
+    for (let numDiv of listOfNumDivs) {
+        console.log(numDiv, "en cell");
+        numDiv.addEventListener("click", function () {
+            console.log("en cell klickades på");
+            numDiv.classList.remove("gridCellsColor");
+            numDiv.classList.add("cellEventColor");
+            sumOfMarked += Number(numDiv.textContent);
+            console.log(sumOfMarked);
+            pSumOfMarked.textContent = sumOfMarked;
+        })
+    }
+}
 
 //pSumOfMarked.textContent = "";
 // event på create-knappen med ett funktionsanrop till sumAllCurrentCells 
@@ -32,6 +48,7 @@ ButtonCreate.addEventListener("click", function () {
     pSumOfAll.textContent = "";
     pSumOfAll.textContent = sumAllCurrentCells();
 
+    pSumOfMarked.textContent = sumMarkedDivsAndChangeTheirColor();
 
 })
 
@@ -40,4 +57,24 @@ ButtonCreate.addEventListener("click", function () {
 document.addEventListener("DOMContentLoaded", () => {
     console.log(document.body.innerHTML); // Visar hela body-strukturen
 });
+
+// För att summera måste vi ha:
+// 1. En variabel på vilken summan ska sparas. Denna tilldelas värdet 0 från start
+// 2. En loop som går igenom varje node i allCurrentCells och tar en div i taget, kollar 
+// dess textContent och omvandlar det till en siffra (eftersom det är en sträng) och sen 
+// ska siffran tilldelas till variabeln. Detta ska göras tills loopen har gått igenom alla divar
+// 3. När variabeln är "full" så ska den placeras i <p class="pInsideContainer"></p>, med
+// textContent förslagsvis 
+// Allt detta måste ske inom samma "runda" så att summan gäller för de divar som finns på sidan
+// Därför kan <p class="pInsideContainer"></p> behöva tömmas innan den ska fyllas igen med en ny summa
+
+// För att ändra färg på de celler som markeras så ska 
+// 1. ett event ske på cellen med "klick"
+// 2. klassen gridCellsColor ska tas bort
+// 3. klassen cellEventColor ska läggas till
+
+// För att ta bort divarna från sidan vid klick på reset-knappen 
+// 1. Ha ett event på reset
+// 2. Sätta <p class="pInsideContainer">-</p>.textContent = "-"
+
 
