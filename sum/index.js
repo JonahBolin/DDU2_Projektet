192e3a462a26406154b9722c236deeccce1c6725
JonahBolin <jonah.bolin17@gmail.com>
2025-01-05 13:21:21 +0100

    Summering av alla tal (sum)
    Funktionen sumAllCurrentCells får sitt anrop i en event listener för create-knappen och i funktionen hämtas en nodelist med allCurrentCells som skapats. for-of-loopen används för att ta varje cells siffer-innehåll, och spara det på variabeln sumOfAll, som tillslut innehåller summan av alla tal i griden. Innan sumOfAll blir pSumOfAlls textinnehåll så töms elementet så den föregående summan nollställs.

--- sum/index.js
+++ sum/index.js
@@ -1,22 +1,40 @@
 document.addEventListener('DOMContentLoaded', function () {
     createElementsInHeader();
     createGridShell(95);
+
+let pSumOfAll = document.querySelector(".pInsideContainer");
+let pSumOfMarked = document.getElementById("pSumOfMarked");
+console.log(pSumOfMarked);
+
 let ButtonCreate = document.querySelector(".buttonCreate");
 console.log(ButtonCreate);
 
+let resetButton = document.getElementById("resetButton");
+
 function sumAllCurrentCells() {
     const allCurrentCells = document.querySelectorAll(".gridCells");
+    let sumOfAll = 0;
+    const allCurrentCells = document.querySelectorAll(".gridCells"); //nodelist 
     console.log(allCurrentCells);
+
+    for (let cell of allCurrentCells) {
+        console.log(cell.textContent);
+        sumOfAll += Number(cell.textContent);
+        console.log(sumOfAll);
+    }
+    return sumOfAll;
 }
 
 
-
-
-
-
-// anrop
+//pSumOfMarked.textContent = "";
+// event på create-knappen med ett funktionsanrop till sumAllCurrentCells 
 ButtonCreate.addEventListener("click", function () {
-    sumAllCurrentCells();
+    pSumOfAll.textContent = "";
+    pSumOfAll.textContent = sumAllCurrentCells();
+
+
+})
+
 })
 
 document.addEventListener("DOMContentLoaded", () => {
