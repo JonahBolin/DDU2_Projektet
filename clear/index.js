159b5754787eeeff06c2ff8255980e8f3a3e004e
JonahBolin <jonah.bolin17@gmail.com>
2025-01-13 18:46:37 +0100

    Funktioner och events (clear)
    Vid klick på create-button anropas funktionen collectDivs, i vilken en nodelist sparas med skapta divar och med forEach-metoden så skickas resp. div som argument till funktionen startRoundForDivChanges. dataset.phase resp. dataset.cycle används för att hålla koll på vilken fas/cykel som diven befinner sig i. Det finns tre event-listeners för diven. I resp. anropas funktioner med olika style-ändringar beroende på om diven befinner sig i cykel 1 eller 2. I varje funktion uppdateras phase och cycle, samt diven får en ny färg/textinnehåll, samt att diven sparas i arrayen currentFilledDivs, i fall den inte redan finns med. Vid klick på clearButton anropas funktionen clearFilledDivs, i vilken divarna i currentFilledArrays byter färg och får en random siffra. phase  och cycle återställs så även om diven inte gått igenom alla möjliga events så ska den börja om "från början" nästa gång den behandlas. I resetArrays-funktionen töms currentFilledDivs.

--- clear/index.js
+++ clear/index.js
@@ -1,4 +1,139 @@
 document.addEventListener('DOMContentLoaded', function () {
     createElementsInHeader();
     createGridShell(95);
-})
+})const createDivsButton = document.querySelector(".buttonCreate");
+const clearButton = document.getElementById("clearButton");
+let currentFilledDivs = [];
+let previousFilledDivs = [];
+let divHasAFillChange = false;
+
+
+function collectDivs() {
+    const numDivs = document.querySelectorAll(".gridCells");
+
+    numDivs.forEach((div) => {
+        startRoundForDivChanges(div);
+    });
+}
+
+function startRoundForDivChanges(div) {
+
+    div.dataset.phase = "idle";
+    console.log(`${div.dataset.phase}, diven har inte utsatts för något event ännu`);
+    div.dataset.cycle = "1";
+
+    div.addEventListener("mouseenter", function () {
+        console.log(div.textContent, "diven som behandlas nu");
+        mouseEnter(div);
+    });
+    div.addEventListener("click", function () {
+        mouseClick(div);
+    });
+    div.addEventListener("mouseleave", function () {
+        mouseLeave(div)
+    });
+}
+
+function mouseEnter(div) {
+    if (div.dataset.phase === "idle" || div.dataset.phase === "mouseleave") {
+
+        let cycle = div.dataset.cycle;
+
+        if (cycle === "1") {
+            div.classList.replace("gridCellsColor", "mouseoverColor"); //gul
+            console.log(cycle, "denna cykeln är vi på nu");
+        } else if (cycle === "2") {
+            console.log(cycle, "denna cykeln är vi på nu");
+            div.classList.replace("mouseleaveColor", "clickColor"); //guld
+        }
+
+        div.dataset.phase = "mouseenter";
+        console.log(`mouseenter är triggad, fasen = ${div.dataset.phase}`);
+
+        storeNewFilledDivs(div);
+    }
+}
+
+function mouseClick(div) {
+    if (div.dataset.phase === "mouseenter") {
+
+        let cycle = div.dataset.cycle;
+
+        if (cycle === "1") {
+            div.classList.replace("mouseoverColor", "clickColor"); //guld
+            div.textContent = "";
+            console.log(cycle, "denna cykeln är vi på nu");
+        } else if (cycle === "2") {
+            div.classList.replace("clickColor", "mouseoverColor"); //gul
+            if (!div.classList.contains("gridCellsColor")) {
+                div.textContent = createRandomNumber();
+                console.log(cycle, "denna cykeln är vi på nu");
+            }
+
+        }
+        div.dataset.phase = "click";
+        console.log(`click är triggad, fasen = ${div.dataset.phase}`);
+
+        storeNewFilledDivs(div);
+    }
+}
+
+function mouseLeave(div) {
+    if (div.dataset.phase === "click") {
+
+        let cycle = div.dataset.cycle;
+
+        if (cycle === "1") {
+            div.classList.replace("clickColor", "mouseleaveColor");
+            div.dataset.cycle = "2";
+            console.log(cycle, "denna cykeln är vi på nu");
+        } else if (cycle === "2") {
+            div.classList.replace("mouseoverColor", "gridCellsColor");
+            div.dataset.cycle = "1";
+            console.log(cycle, "denna cykeln är vi på nu");
+        }
+        div.dataset.phase = "mouseleave";
+        console.log(`mouseleave är triggad, fasen = ${div.dataset.phase}`);
+
+        storeNewFilledDivs(div);
+    }
+}
+
+function storeNewFilledDivs(div) {
+    if (!currentFilledDivs.includes(div)) {
+        currentFilledDivs.push(div);
+        console.log(currentFilledDivs, "array med divar som inte förekommit tidigare");
+    }
+}
+
+
+function resetArrays(currentArray) {
+    previousFilledDivs = currentArray
+    currentArray = [];
+    console.log(currentArray);
+}
+
+function clearFilledDivs() {
+    console.log(currentFilledDivs);
+    if (currentFilledDivs.length > 0) {
+        currentFilledDivs.forEach((filledDiv) => {
+            if (!filledDiv.classList.contains("gridCellsColor")) {
+                if (filledDiv.classList.contains("mouseoverColor", "clickColor", "mouseleaveColor")); {
+                    filledDiv.textContent = createRandomNumber();
+                    filledDiv.classList.remove("mouseoverColor", "clickColor", "mouseleaveColor");
+                    filledDiv.classList.add("gridCellsColor");
+
+                    filledDiv.dataset.phase = "idle";
+                    filledDiv.dataset.cycle = "1";
+
+                    console.log(filledDiv.dataset.phase, filledDiv.dataset.cycle, "divarna är återställd");
+                    console.log(filledDiv.classList.contains("mouseleaveColor"));
+                }
+            }
+        })
+        resetArrays(currentFilledDivs);
+    }
+}
+
+createDivsButton.addEventListener("click", collectDivs);
+clearButton.addEventListener("click", clearFilledDivs);
