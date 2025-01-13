19814cd86e1ae2c0907dfcbb0cf6c526ae0d1f7e
JonahBolin <jonah.bolin17@gmail.com>
2025-01-13 18:31:32 +0100

    Hitta matchningar (AddUpTo)
    Vid tryck på addUpButton anropas funktionen findAddedUpCells i vilken de genererade divarna hämtas och sparas. Ett villkor kollar om inputValue inte är null, vilket betyder att input har ett värde inskrivet. Om det har det så tilldelas inputSum värdet av inputValue. Arrayen previousMatchedDivs som innehåller förra omgångens matchade divar kollas igenom och divarna i den byter tillbaka sin färg, för att förhindra att de markeras tillsammans med de nya matchade divarna. I de nästade looparna sparas divA och divB för att sedan jämföras. Om divA + divB blir inputSum så har två divar matchats och ska få ny färg. foundMatchesForInput blir true och looparna stoppas. Arrayen currentMatchedDivs fylls med divA och divB och innan nästa omgång tilldelas previousMatchedDivs den nuvarande omgångens matchade divar.

--- addUpTo/index.js
+++ addUpTo/index.js
@@ -1,4 +1,71 @@
 document.addEventListener('DOMContentLoaded', function () {
     createElementsInHeader();
     createGridShell(95);
+})const addingInput = document.getElementById("addingInput");
+const addUpButton = document.getElementById("addUpToButtonColor");
+let previousMatchedDivs = [];
+let inputValue = null;
+
+function findAddedUpCells() {
+    const numDivsAvailableCurrently = document.querySelectorAll(".gridCells");//nodeList
+    console.log(numDivsAvailableCurrently);
+
+    if (inputValue !== null) {
+        const inputSum = inputValue;
+        let foundMatchesForInput = false;
+
+        if (previousMatchedDivs.length > 0) {
+            previousMatchedDivs.forEach((matchedDiv) => {
+                matchedDiv.classList.replace("addedUpDivsColor", "gridCellsColor");
+            })
+        }
+
+        for (let i = 0; i < numDivsAvailableCurrently.length; i++) {
+            console.log(numDivsAvailableCurrently[i], "Div i");
+            let divA = Number(numDivsAvailableCurrently[i].textContent);
+            console.log(divA);
+
+            for (let j = 0; j < numDivsAvailableCurrently.length; j++) {
+                console.log(numDivsAvailableCurrently[j], "Div j");
+                let divB = Number(numDivsAvailableCurrently[j].textContent);
+                console.log(divB);
+
+                if (i === j) continue;
+
+                if ((divA + divB) === inputSum) {
+                    let currentMatchedDivs = [];
+                    let firstMatchedDiv = numDivsAvailableCurrently[i];
+                    let secondMatchedDiv = numDivsAvailableCurrently[j];
+
+                    currentMatchedDivs.push(firstMatchedDiv, secondMatchedDiv);
+                    console.log(currentMatchedDivs, "Detta är en array med matchade divar");
+
+                    console.log(`${divA} plus ${divB} blir ${inputSum}`);
+                    firstMatchedDiv.classList.replace("gridCellsColor", "addedUpDivsColor");
+                    secondMatchedDiv.classList.replace("gridCellsColor", "addedUpDivsColor");
+
+                    foundMatchesForInput = true;
+                    previousMatchedDivs = currentMatchedDivs;
+                    break;
+                }
+            }
+            if (foundMatchesForInput) break;
+        }
+    }
+}
+
+addingInput.addEventListener("blur", function (e) {
+    const inputContent = parseInt(e.target.value, 10);
+    if (!isNaN(inputValue)) {
+        inputValue = inputContent;
+        console.log(inputValue, "Detta är värdet från input och är en siffra");
+    }
 })
+
+addUpButton.addEventListener("click", function () {
+    console.log("AddUpButton klickades på");
+    findAddedUpCells();
+})
+
+
+
